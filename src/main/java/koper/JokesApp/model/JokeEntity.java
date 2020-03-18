fe01836359410dbfb688a51206102387114671ea
idan koper <idankoper@gmail.com>
2020-03-18 18:01:14 +0200

    Using Thymeleaf to display the joke, Comment the entity model that support using database.

--- src/main/java/koper/JokesApp/model/JokeEntity.java
+++ src/main/java/koper/JokesApp/model/JokeEntity.java
@@ -1,48 +1,46 @@
-package koper.JokesApp.model;
-
-
-import com.sun.istack.NotNull;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-
-@Entity
-public class JokeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    @NotEmpty
-    private String joke;
-
-    @NotEmpty
-    private String author;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getJoke() {
-        return joke;
-    }
-
-    public void setJoke(String joke) {
-        this.joke = joke;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-}
+//package koper.JokesApp.model;
+//
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.validation.constraints.NotEmpty;
+//
+//@Entity
+//public class JokeEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long id;
+//
+//    @NotEmpty
+//    private String joke;
+//
+//    @NotEmpty
+//    private String author;
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getJoke() {
+//        return joke;
+//    }
+//
+//    public void setJoke(String joke) {
+//        this.joke = joke;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//}
